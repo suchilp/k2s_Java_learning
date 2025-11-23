bfe7159f2810cfa501495cd1118907ed4a8c4614
shashwateesahu <shashwatee.sahu2000@gmail.com>
2025-11-22 22:09:49 -0500

    Swati

---
+++ Hibernate1/src/main/java/org/swati/Main.java
@@ -0,0 +1,11 @@
+package org.swati;
+
+public class Main {
+    int x=5;
+    public static void main (String[]args){
+        Main myobj=new Main();
+        Main myobj2=new Main();
+        System.out.println(myobj.x);
+        System.out.println(myobj2.x);
+    }
+}
