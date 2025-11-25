f9acafaf7c1fc91bf735100a247e77d31dfb18b8
shashwateesahu <shashwatee.sahu2000@gmail.com>
2025-11-24 21:08:48 -0500

    Array length

---
+++ Hibernate1/src/main/java/org/swati/Main2.java
@@ -0,0 +1,8 @@
+package org.swati;
+
+public class Main2 {
+    public static void main(String[] args) {
+        String[] animals={"Cow","Dog","Cat"};
+        System.out.println(animals.length);
+    }
+}
