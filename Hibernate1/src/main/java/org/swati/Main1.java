dd01b4f78ff0787030b6ec77106bbe7a891d332a
shashwateesahu <shashwatee.sahu2000@gmail.com>
2025-11-24 21:03:49 -0500

    Index number

---
+++ Hibernate1/src/main/java/org/swati/Main1.java
@@ -0,0 +1,9 @@
+package org.swati;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        String[] animals={"Dog","Cat","Rabbit","Cow"};
+        System.out.println(animals[1]);
+    }
+}
