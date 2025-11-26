813ca24b749f03907984650d54048b371b0a1dc9
shashwateesahu <shashwatee.sahu2000@gmail.com>
2025-11-25 19:07:21 -0500

    OOPS java

--- Hibernate1/src/main/java/org/swati/Oops.java
+++ Hibernate1/src/main/java/org/swati/Oops.java
@@ -1,1 +1,21 @@
+package org.swati;
 
+ class Pen{
+    String color;
+    String type;
+    public void write(){
+        System.out.println("Writting something");
+    }
+}
+
+
+
+
+public class Oops{
+public static void main(String args[]){
+    Pen p=new Pen();
+    p.color="blue";
+    p.type="gel";
+    p.write();
+}
+}
