5309cbccdf15b808ec1372e094699429378aec4b
shashwateesahu <shashwatee.sahu2000@gmail.com>
2025-11-25 19:47:20 -0500

    Encapsulation(set and get).

---
+++ Hibernate1/src/main/java/org/swati/Dog.java
@@ -0,0 +1,34 @@
+package org.swati;
+
+public class Dog {
+   private String color;
+   private String name;
+   private int age;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
