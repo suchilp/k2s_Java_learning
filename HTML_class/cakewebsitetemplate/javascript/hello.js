339f957da1a98a7201526085c077c0ed762415a3
suchil pradhan <pradhan.suchil@gmail.com>
2025-11-26 23:32:10 -0500

    extra

---
+++ HTML_class/cakewebsitetemplate/javascript/hello.js
@@ -0,0 +1,17 @@
+var message = 'Hello, World!';
+console.log(message);
+function areaCircle(radius) {
+    return Math.PI * radius * radius;
+}
+var message1 = 'Hello, World!';
+console.log(message1);
+function areaCircle1(radius) {
+    return Math.PI * radius * radius;
+}
+
+
+function animals()
+{
+
+    var animals:String =['Tiger']
+}
