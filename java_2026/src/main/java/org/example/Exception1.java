5bf97f35c72ed1db5a416c1e12f0a42cfed25140
suchil pradhan <pradhan.suchil@gmail.com>
2025-12-26 14:30:23 -0500

    'test'

--- java_2026/src/main/java/org/example/Exception1.java
+++ java_2026/src/main/java/org/example/Exception1.java
@@ -1,2 +1,28 @@
-package org.example;public class Exception1 {
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exception1 {
+    public static void main(String[] args) {
+
+        File f = new File("C://a.text");
+        try {
+            FileReader obj=   new FileReader(f);
+        }
+
+
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
