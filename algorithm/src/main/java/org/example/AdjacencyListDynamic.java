8fee87a17771120d746e8c48f11e6817aa4f3c43
suchil pradhan <pradhan.suchil@gmail.com>
2026-01-22 19:52:41 -0500

    project added

---
+++ algorithm/src/main/java/org/example/AdjacencyListDynamic.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdjacencyListDynamic {
+    int V = 5;
+    private List<List<Integer>> adjList;
+    AdjacencyListDynamic() {
+        adjList = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+    }
+    public static void main(String[] args) {
+        AdjacencyListDynamic obj = new AdjacencyListDynamic();
+        int[][] edges = {
+                {0, 1}, {0, 2,}, {0, 3},
+                {1, 2}, {1, 4},
+                {2, 3}, {2, 4},
+                {3, 4},
+                {4, 3}, {4, 1}, {4, 2}
+        };
+        for (int[] e : edges) {
+            obj.addEdge(e[0], e[1]);
+        }
+        obj.printGraph();
+    }
+
+    void printGraph() {
+        for (int i = 0; i < V; i++) {
+            System.out.println(adjList.get(i));
+        }
+    }
+    void addEdge(int src, int dest) {
+        adjList.get(src).add(dest);
+    }
+}
